b3d35bc21a7a9597804680cb8be500894d1efd18
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2020-11-26 10:55:53 +0700

    Create pheptinh.c

---
+++ pheptinh.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+  //khai ao bien a, b kieu it
+  int a, b;
+  a=3;
+  b=5;
+  int tong, hieu, tich, thuong;
+  tong = a + b;
+  hieu = a - b;
+  tich = a * b;
+  thuong = a / b;
+  print("a +b = %d, a - b = %d, a * b= %d, a / b = $f, tong, hieu, tich, thuong")
+  
+}
